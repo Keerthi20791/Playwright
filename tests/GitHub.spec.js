cc7b2207f2c056c817c56580eb64d4a6ddd652d4
Praveen <praveen.k@xnara.com>
2025-08-19 20:44:03 +0530

    dashboardPage

--- tests/GitHub.spec.js
+++ tests/GitHub.spec.js
@@ -2,5 +2,14 @@
 
 // Requirement: Work on dashboardPage test cases
 // 1. Clone the repository inside the local system - git clone <repo url>
-// git clone https://github.com/Praveenroy05/IshaPWJuly.git
-// 2. Install all the dependencies
+// git clone https://github.com/Praveenroy05/IshaPWJuly.git - create a local git as well
+// 2. Install all the dependencies - npm install
+// 3. Check the branches that are available inside the github - git branch
+// 4. Before you start working on the requirement you can create a new feature branch - dashboardPage - git checkout -b branchName
+// git checkout -b dashboardPage
+// 5. You will start working on this branch for writing the test cases
+// 6. Add the code to the local git - "git add ." - it will add all the files to the local git
+// git add fileName - a specific file to add it inside the local
+// 7. Commit the changes - git commit -m "commit message"
+// 8. git push 
+
